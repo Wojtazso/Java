ba6b642bf0ed9882250af07dfd246a0c06817a38
Wojtazso <wojciechpizun1920@gmail.com>
2025-03-04 18:06:01 +0100

    Add files via upload

---
+++ LabydoSkonczenia/LabydoSkonczenia/Laby1/src/ZadaniaLaby3/Zadania.java
@@ -0,0 +1,9 @@
+package ZadaniaLaby3;
+
+public class Zadania {
+    public static void zad1(){}
+    public static void zad2(){}
+    public static void zad3(){}
+    public static void zad4(){}
+    public static void zad5(){}
+}
