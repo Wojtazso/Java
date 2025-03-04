f685e8d82b8bee8b5c692204eaab07bede2a0dfe
Wojtazso <wojciechpizun1920@gmail.com>
2025-03-04 18:07:07 +0100

    Delete LabydoSkonczenia/LabydoSkonczenia/Laby1 directory

--- LabydoSkonczenia/LabydoSkonczenia/Laby1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Witaj, program służy do pokazywania rozwiazań zadań z Labolatoriów z programowania obiektowego.\nWybierz numer labolatoriów, następnie numer zadania.");
-        System.out.println("Po wybraniu zadania zostanie wyświetlona treść zadania, program wyświetli czego potrzebuje, nastepnie wynik zadania lub sam wynik zadania bez podawnia danych.");
-
-        WyborLabow run = new WyborLabow();
-        run.WyborLabowy();
-    }
-}
