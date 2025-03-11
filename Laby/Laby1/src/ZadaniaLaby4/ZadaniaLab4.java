dd41ad01824e07a8ead7a11633fdde3da895b8ac
Wojtazso <wojciechpizun1920@gmail.com>
2025-03-11 23:24:26 +0100

    Delete Lab1-2/LabydoSkonczenia/Laby1 directory

--- Laby/Laby1/src/ZadaniaLaby4/ZadaniaLab4.java
+++ Laby/Laby1/src/ZadaniaLaby4/ZadaniaLab4.java
@@ -1,11 +1,11 @@
 package ZadaniaLaby4;
 
 public class ZadaniaLab4 {
-    protected  void zad1(){}
-    protected  void zad2(){}
-    protected  void zad3(){}
-    protected  void zad4(){}
-    protected  void zad5(){}
-    protected  void zad6(){}
-    protected  void zad7(){}
+    protected  static void zad1(){}
+    protected  static void zad2(){}
+    protected  static void zad3(){}
+    protected  static void zad4(){}
+    protected  static void zad5(){}
+    protected  static void zad6(){}
+    protected  static void zad7(){}
 }
