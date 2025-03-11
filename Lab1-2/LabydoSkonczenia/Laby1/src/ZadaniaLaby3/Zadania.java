8443ad746b1d7b201dae82ffd2a247bb56cc2ec6
Wojtazso <wojciechpizun1920@gmail.com>
2025-03-11 23:24:26 +0100

    Delete Lab1-2/LabydoSkonczenia/Laby1 directory

--- Lab1-2/LabydoSkonczenia/Laby1/src/ZadaniaLaby3/Zadania.java
+++
@@ -1,9 +0,0 @@
-package ZadaniaLaby3;
-
-public class Zadania {
-    public static void zad1(){}
-    public static void zad2(){}
-    public static void zad3(){}
-    public static void zad4(){}
-    public static void zad5(){}
-}
